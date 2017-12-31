3dcb86ba76b0ceea522a43214a3239afb72a80b5
Qian Li <lqustc@gmail.com>
2017-12-31 10:54:57 -0800

    seed the feature table

---
+++ Migrations/20171231183812_SeedFeatureTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CarDealer.Migrations
+{
+    public partial class SeedFeatureTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO CarDealer.Features (Name) VALUES('Color - Blue');");
+            migrationBuilder.Sql("INSERT INTO CarDealer.Features (Name) VALUES('Color - Black');");
+            migrationBuilder.Sql("INSERT INTO CarDealer.Features (Name) VALUES('Color - Red');");
+            migrationBuilder.Sql("INSERT INTO CarDealer.Features (Name) VALUES('Auto Pilot');");
+            migrationBuilder.Sql("INSERT INTO CarDealer.Features (Name) VALUES('Sunroof');");
+            migrationBuilder.Sql("INSERT INTO CarDealer.Features (Name) VALUES('Moonroof');");
+            migrationBuilder.Sql("INSERT INTO CarDealer.Features (Name) VALUES('360 Degree Roof');");
+            migrationBuilder.Sql("INSERT INTO CarDealer.Features (Name) VALUES('Rear Radar & Camera');");
+            migrationBuilder.Sql("INSERT INTO CarDealer.Features (Name) VALUES('Side Radar & Camera');");
+            migrationBuilder.Sql("INSERT INTO CarDealer.Features (Name) VALUES('CamCorder');");
+            migrationBuilder.Sql("INSERT INTO CarDealer.Features (Name) VALUES('6 Gear Auto-transmission');");
+            migrationBuilder.Sql("INSERT INTO CarDealer.Features (Name) VALUES('Luxury Stereo System');");
+            migrationBuilder.Sql("INSERT INTO CarDealer.Features (Name) VALUES('Leather Seats');");
+            migrationBuilder.Sql("INSERT INTO CarDealer.Features (Name) VALUES('Heating Seats');");
+            migrationBuilder.Sql("INSERT INTO CarDealer.Features (Name) VALUES('Alloy Wheel - 17');");
+            migrationBuilder.Sql("INSERT INTO CarDealer.Features (Name) VALUES('Alloy Wheel - 19');");
+            migrationBuilder.Sql("INSERT INTO CarDealer.Features (Name) VALUES('Alloy Wheel - 21');");
+            migrationBuilder.Sql("INSERT INTO CarDealer.Features (Name) VALUES('Remote Start');");
+            migrationBuilder.Sql("INSERT INTO CarDealer.Features (Name) VALUES('Central Control System');");
+            migrationBuilder.Sql("INSERT INTO CarDealer.Features (Name) VALUES('Disk Brake');");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM CarDearler.Features;");
+        }
+    }
+}
