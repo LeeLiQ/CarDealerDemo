939d9619f1b0d9631732b039ba669406170149c3
Qian Li <lqustc@gmail.com>
2017-12-31 21:25:45 -0800

    sovled ng g c issue

--- ClientApp/app/app.server.module.ts
+++ ClientApp/app/app.server.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
-import { AppModuleShared } from './app.shared.module';
+import { AppModuleShared } from './app.module';
 import { AppComponent } from './components/app/app.component';
 
 @NgModule({
-    bootstrap: [ AppComponent ],
+    bootstrap: [AppComponent],
     imports: [
         ServerModule,
         AppModuleShared
