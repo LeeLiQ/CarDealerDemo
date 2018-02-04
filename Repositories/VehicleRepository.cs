0af07916156657b280b81666020997335b63211b
Qian Li <lqustc@gmail.com>
2018-02-04 15:38:34 -0800

    Added vehicle repository and will use it to resolve code redundency in the vehicle controller.

---
+++ Repositories/VehicleRepository.cs
@@ -0,0 +1,27 @@
+using System.Threading.Tasks;
+using CarDealer.Models;
+using CarDealer.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarDealer.Repositories
+{
+    public class VehicleRepository
+    {
+        private readonly CarDealerDbContext context;
+        public VehicleRepository(CarDealerDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Vehicle> GetVehicle(int id)
+        {
+            return
+                await context.Vehicles
+                        .Include(v => v.Features)
+                            .ThenInclude(v => v.Feature)
+                        .Include(v => v.Model)
+                            .ThenInclude(m => m.Make)
+                        .SingleOrDefaultAsync(v => v.Id == id);
+        }
+    }
+}
