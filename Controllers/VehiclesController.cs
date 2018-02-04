5ce144103717c3d5e85fd099d156b076d658c575
Qian Li <lqustc@gmail.com>
2018-02-04 15:29:56 -0800

    Make change to make the return of update/create more informative with not just ids

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -47,7 +47,14 @@ namespace CarDealer.Controllers
             context.Vehicles.Add(vehicle);
             await context.SaveChangesAsync();
 
-            var result = mapper.Map<Vehicle, SaveVehicleResource>(vehicle);
+            vehicle = await context.Vehicles
+                            .Include(v => v.Features)
+                                .ThenInclude(v => v.Feature)
+                            .Include(v => v.Model)
+                                .ThenInclude(m => m.Make)
+                            .SingleOrDefaultAsync(v => v.Id == vehicle.Id);
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
 
             return Ok(result);
         }
@@ -58,7 +65,12 @@ namespace CarDealer.Controllers
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
-            var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+            var vehicle = await context.Vehicles
+                            .Include(v => v.Features)
+                                .ThenInclude(v => v.Feature)
+                            .Include(v => v.Model)
+                                .ThenInclude(m => m.Make)
+                            .SingleOrDefaultAsync(v => v.Id == id);
 
             if (vehicle == null)
                 return NotFound();
@@ -69,7 +81,7 @@ namespace CarDealer.Controllers
 
             await context.SaveChangesAsync();
 
-            var result = mapper.Map<Vehicle, SaveVehicleResource>(vehicle);
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
 
             return Ok(result);
         }
